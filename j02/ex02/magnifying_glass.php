cb8aaf7a494e3559afae8e75b1de05806d2efa56
Benjamin WAN NAN WO <bwan-nan@e1r5p17.42.fr>
2019-03-27 13:03:47 +0100

    Modified magnifying_glass

--- j02/ex02/magnifying_glass.php
+++ j02/ex02/magnifying_glass.php
@@ -1,22 +1,28 @@
 #!/usr/bin/php
 <?php
 
-function		ft_upper($matches)
+function		upper_inside($matches)
 {
 	return (str_replace($matches[1], strtoupper($matches[1]), $matches[0]));
 }
 
+function		upper_a($matches)
+{
+	$matches[0] = preg_replace_callback('/>(.*?)</si', 'upper_inside', $matches[0]);
+	$matches[0] = preg_replace_callback('/title="(.*?)"/', 'upper_inside', $matches[0]);
+	return ($matches[0]);
+}
+
 if ($argc == 2)
 {
+	$whole_page = '';
 	if (file_exists($argv[1]))
 	{
 		$fd = fopen($argv[1], 'r');
 		while ($line = fgets($fd))
-		{
-				$line = preg_replace_callback('/<a.*?title="(.*?)">/', 'ft_upper', $line); 
-				$line = preg_replace_callback('/<a.*?>(.*?)</', 'ft_upper', $line);  
-				echo $line;
-		}
+			$whole_page .= $line;
+		$whole_page = preg_replace_callback('/(<a .*?<\/a>)/si', 'upper_a', $whole_page);  
+		echo $whole_page;
 	}
 	else
 		echo "File does not exist.\n";
