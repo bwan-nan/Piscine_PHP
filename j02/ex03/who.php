b4b77f8ffc4fa4e895a4fa4eeee23f267b7614f1
Benjamin WAN NAN WO <bwan-nan@e1r5p17.42.fr>
2019-03-28 12:29:36 +0100

    Deleted binary garbage in output ex03

--- j02/ex03/who.php
+++ j02/ex03/who.php
@@ -7,12 +7,12 @@ while ($data = fread($fd, 628))
 {
 	if (strlen($data) == 628)
 	{
-		$data = unpack("a256user/a4id/a32line/ipid/itype/itime", $data);
+		$data = unpack("a8user/a248garbage/a4id/a7line/a25garbage/ipid/itype/itime", $data);
 		//print_r($data);
 		if ($data['type'] == 7)
 		{
 			$date = ucfirst(strftime('%h %d %H:%M', $data['time'])); 
-			$result[] = $data['user']." ".$data['line']."  ".$date;
+			$result[] = $data['user']." ".$data['line']."  ".$date." ";
 		}
 	}
 }
