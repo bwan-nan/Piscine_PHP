635339e016eff69fa0351aa280ea52c2f328f9b3
Benjamin WAN NAN WO <bwan-nan@e1r5p17.42.fr>
2019-03-28 19:39:40 +0100

    5 day cookie oin d03 ex03

--- j03/ex03/cookie_crisp.php
+++ j03/ex03/cookie_crisp.php
@@ -1,8 +1,8 @@
 <?php
-if ($_GET['action'] == 'set')
-	setcookie($_GET['name'], $_GET['value'], time() + 86400);
-else if ($_GET['action'] == 'get')
+if ($_GET['action'] === 'set')
+	setcookie($_GET['name'], $_GET['value'], time() + 86400 * 5);
+else if ($_GET['action'] === 'get')
 	echo $_COOKIE[$_GET['name']]."\n";
-else if ($_GET['action'] == 'del')
+else if ($_GET['action'] === 'del')
 	setcookie($_GET['name'], NULL, -1);
 ?>
